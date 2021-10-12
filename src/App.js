0f60cb2c0f886fe02bdf0cc848a2c0ba44c50ac5
Santo <90331134+lastdark@users.noreply.github.com>
2021-10-12 10:13:09 +0200

    add toggle obj

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
-import { useState, useEffect } from "react";
+
 import Basket from "./Basket";
 import Books from "./Books";
 import {useReducer} from "react";
 import Navigation from "./Navigation";
-const INITIAL_STATE = {opned:false};
+import booksMock from "./mocks/books";
+const INITIAL_STATE = {
+    basket: {
+        items: [],
+        totalPrice: 0,
+        quantity: 0,
+        opened: false,
+    },
+    filters: {
+        word: "",
+        category: "All",
+    },
+    books: {
+        booksMock,
+        categories: ["All", "Design", "Mobile", "Ux", "DevOps", "Essentials"],
+    },
+};
+
 
 function reducer(state, action) {
-    console.log(action);
+
     switch (action.type) {
         case "TOGGLE_BASKET":
-            return {...state ,opned: !state.opned};
+            return {...state , basket: {...state.basket, opened: !state.basket.opened}};
 
         default:
             return state;
     }
 }
+
+
 function App() {
 
     const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
 
-    console.log(state.opned);
+    console.log(state.basket);
 
   return (
     <div className='App'>
       <Navigation   dispatch={dispatch} />
       <Books />
-      <Basket dispatch={dispatch} opened={state.opned} />
+      <Basket dispatch={dispatch} opened={state.basket.opened} />
     </div>
   );
 }
